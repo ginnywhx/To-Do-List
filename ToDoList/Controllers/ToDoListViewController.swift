20fc7f9f71ecd25837c1a0973dd3f0f5db74b66c
Ginny Wan <zctywan@ucl.ac.uk>
2019-07-24 17:01:05 +0100

    Added App Icons

--- ToDoList/Controllers/ToDoListViewController.swift
+++ ToDoList/Controllers/ToDoListViewController.swift
@@ -10,17 +10,25 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    var itemArray = ["Finish an Ios App", "LBS Coursework", "Read Entrepreneurship-related Books"]
+    var itemArray = [Item]()
 
     //add defaults to make sure that itemArray and items added to the item Array would be stored
     let defaults = UserDefaults.standard
     
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        print(dataFilePath)
+
+        loadItems()
+        
         //ensure that defaults are saved even when teh app accidentally recedes to the background or terminates 
-        if let items = defaults.array(forKey: "ToDoListArray" ) as? [String] {
-            itemArray = items
-        }
+//        if let items = defaults.array(forKey: "ToDoListArray" ) as? [Item] {
+//            itemArray = items
+//        }
     }
     
     //MARK - Tableview Datasource Methods: set up the table framework
@@ -31,7 +39,11 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done == true ? .checkmark : .none
         
         return cell
     }
@@ -48,12 +60,17 @@ class ToDoListViewController: UITableViewController {
         //What happens when the user clicks the Alert/ Add item button
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
-            self.itemArray.append(textField.text!)
             
-            //messages saved to defaults in plist (always have keys and values)
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            let newItem = Item()
             
-            self.tableView.reloadData()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            
+//            //messages saved to defaults in plist (always have keys and values)
+//            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            
+            self.saveItems()
         }
         
         alert.addTextField { (alertTextField) in
@@ -72,22 +89,53 @@ class ToDoListViewController: UITableViewController {
     //MARK - Tableview Data Delegate Methods: what to do with the table rows and columns once selected
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //shows which row I have selected
-//        print(itemArray[indexPath.row])
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        //save this boolean info to the info.plist
+        saveItems()
         
         //make the grey area underneath selected row fade away afrer selection
-       tableView.deselectRow(at: indexPath, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)
         
         //checkmark next to the cell selected
-        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
+//        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        } else {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
+//        
     }
     
     
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+            
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+        tableView.reloadData()
+    }
     
+    func loadItems() {
+    
+        if let data = try? Data(contentsOf: dataFilePath!) {
+        
+            let decoder = PropertyListDecoder()
+            
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error).")
+            }
+        }
+        
+    }
 
 }
 
